375bef246f87cdbb6f5781e32c411adca600c9bd
Wieland Hoffmann <themineo@gmail.com>
2013-10-29 19:13:31 +0100

    Add documentation to run.py

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python2
+"""
+This is a bot to automatically add MBIDs to Wikidata pages.
+
+Usage: python2 run.py [options]
+
+Command line options:
+
+-createtable: Create the table storing the processed MBIDs
+-dryrun: Don't write anything on the server
+-entites: A comma-separated list of entity types for which MBIDs are to be
+          added to their Wikidata pages. Example: `-entities:artist,work,place`
+-limit:x: Only handle x entities of *each* type
+"""
 from bot.common import mainloop
 mainloop()
